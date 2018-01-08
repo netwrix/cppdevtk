e6a79249e80db2d2f1eac0bfbd07f3006eabd22c
Cristian ANITA <cristian_anita@yahoo.com>
2018-01-08 19:10:59 +0200

    updated

--- src/base/res/tr/tr_lt.ts
+++ src/base/res/tr/tr_lt.ts
@@ -1,4 +1,35 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="lt_LT" sourcelanguage="en_US">
+<context>
+    <name>cppdevtk_info</name>
+    <message>
+        <source>CoSoSys</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CoSoSys Ltd.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copyright (C) 2015 - 2018 CoSoSys Ltd. All rights reserved. info@cososys.com.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TM CoSoSys Ltd.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CppDevTk</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>C++ Development Toolkit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CppDevTk is a collection of libraries for C++ development.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
